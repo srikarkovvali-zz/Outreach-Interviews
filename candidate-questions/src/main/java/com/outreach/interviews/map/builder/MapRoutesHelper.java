2f51de8013eddfb32bd8c08617f04a70e66815e9
patrique.legault <patrique.legault@uottawa.ca>
2019-05-13 09:22:46 -0400

    Replacing the other key

--- candidate-questions/src/main/java/com/outreach/interviews/map/builder/MapRoutesHelper.java
+++ candidate-questions/src/main/java/com/outreach/interviews/map/builder/MapRoutesHelper.java
@@ -150,7 +150,7 @@ public class MapRoutesHelper
 		}
 
 		private final String getAPIKey() {
-			return "AIzaSyDUOH7p8tyBp6rgOHKFNpvwFyP0K-65OoU";
+			return "AIzaSyCS_YgXQ_sTcsI71izxnGUB3upy38hv9UA";
 		}
 		
 		private final String getOrigin() {
